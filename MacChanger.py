88bbcb742d25458f3849740eeceb30e78ebe089a
Todd <tringwelski@gmail.com>
2019-11-15 13:22:08 -0500

    Added input support

--- MacChanger.py
+++ MacChanger.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+
+parser = optparse.Optionparser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="newmac", help="New MAC address")
+
+parser.parse_args()
+
 
 interface = raw_input("Interface you want to update mac address, ie: eth0, wlan0 > ")
 newmac = raw_input("New MacAddress x:x:x:y:y:y > ")
