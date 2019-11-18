3646135f44c6b836dbdb03ffda06c1f803f19f24
Ringo <TRingo1@users.noreply.github.com>
2019-11-18 07:55:03 -0500

    Update MacChanger.py

--- MacChanger.py
+++ MacChanger.py
@@ -8,13 +8,12 @@ parser = optparse.Optionparser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="newmac", help="New MAC address")
 
-parser.parse_args()
+(options, arguements) = parser.parse_args()
 
+interface = options.interface
+newmac = options.newmac
 
-interface = raw_input("Interface you want to update mac address, ie: eth0, wlan0 > ")
-newmac = raw_input("New MacAddress x:x:x:y:y:y > ")
 print("[+] Changing Mac address for " + interface + " to " + newmac)
-
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", newmac])
